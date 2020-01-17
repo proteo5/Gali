d594c8621c9feb40b3917910967991153f4e5010
Alfredo Pinto Molina <proteo5@hotmail.com>
2020-01-17 14:01:28 -0800

    Compilador ya crea Modelos

--- Galilu/Program.cs
+++ Galilu/Program.cs
@@ -1,6 +1,12 @@
-﻿using System;
+﻿using Mustache;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Stubble.Core.Builders;
+using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text;
 
 namespace Galilu
 {
@@ -8,17 +14,35 @@ namespace Galilu
     {
         static int Main(string[] args)
         {
-            args = new string[] {".\\test.bc" };
+            args = new string[] { ".\\test.bc" };
             //args = new string[] { ".\\tests.bc" };
             //args = new string[] { ".\\Galilu.exe" };
 
+            //Validation
             int errCode = 0;
             errCode = Validation(args);
             if (errCode > 0) return errCode;
 
-            string newFile = $"{Path.GetDirectoryName(args[0])}{args[0]}.cs";
+            //Get Files
+            string path = Path.GetDirectoryName(args[0]);
+            string fileName = Path.GetFileName(args[0]);
+            string newFile = $"{path}\\{fileName}.cs";
             Console.WriteLine($"New file: {newFile}");
 
+
+            //Get Data to compile
+            string jsonString = CreateJson(args[0]);
+            //dynamic data = JsonConvert.DeserializeObject<dynamic>(jsonString)
+            var data = JObject.Parse(jsonString);
+
+            //Render compiled file
+            string template = Render(".\\Templates\\Entity.cs.template", data);
+            template = template.Replace(";",";\r").Replace("{", "{\r").Replace("}", "}\r");
+
+            //Write file
+            var fileWriter = new System.IO.StreamWriter(newFile);
+            fileWriter.WriteLine(template);
+            fileWriter.Dispose();
 
             return errCode;
         }
@@ -41,8 +65,94 @@ namespace Galilu
                 Console.WriteLine("Err 3, File type not compatible");
                 errCode = 3;
             }
-            
+
             return errCode;
+        }
+
+        static string CreateJson(string FileName)
+        {
+            //List<dynamic> items = JsonConvert.DeserializeObject<List<dynamic>>(contract);
+            string contract;
+            contract = GetFile(FileName);
+
+            string[] contractLines = contract.Split("\r");
+            StringBuilder jsonString = new StringBuilder("{");
+
+            foreach (var item in contractLines)
+            {
+                var itemC = Clean(item).Split(':');
+                if (itemC.Length == 2)
+                {
+                    itemC[0] = Clean(itemC[0]);
+                    itemC[1] = Clean(itemC[1]);
+
+                    if (itemC[0] == "End")
+                    {
+                        jsonString.Append("],");
+                    }
+                    else if (itemC[1] == "")
+                    {
+                        jsonString.Append($"\"{itemC[0]}\":[");
+                    }
+                    else if (itemC[0] == "")
+                    {
+                        var fields = itemC[1].Split(',').Select(x => x.Split(' ')).ToArray();
+                        jsonString.Append("{");
+                        foreach (var item2 in fields)
+                        {
+                            jsonString.Append($"\"{item2[0]}\":\"{item2[1]}\",");
+                        }
+                        jsonString.Append("},");
+                    }
+                    else
+                    {
+                        jsonString.Append($"\"{itemC[0]}\":\"{itemC[1]}\",");
+                    }
+                }
+            }
+            jsonString.Append("}");
+            return jsonString.ToString();
+        }
+
+        static string GetFile(string FileName)
+        {
+            string file;
+            using (StreamReader r = new StreamReader(FileName))
+            {
+                file = r.ReadToEnd();
+            }
+            return file;
+        }
+
+        static string Render(string templateFilePath, JObject data)
+        {
+            string input;
+            string output;
+            using (StreamReader streamReader = new StreamReader(templateFilePath, Encoding.UTF8))
+            {
+                input = streamReader.ReadToEnd();
+            }
+            //var stubble = new StubbleBuilder().Build();
+            //output = stubble.Render(input, data);
+            FormatCompiler compiler = new FormatCompiler();
+            Generator generator = compiler.Compile(input);
+            string result = generator.Render(data);
+            //Console.Out.WriteLine(result);
+            return result;
+        }
+
+        static string Clean(string str)
+        {
+            str = str ?? "";
+            str = str.Replace("\r", "")
+                     .Replace("\n", "")
+                     .Replace("\t", "")
+                     .Replace("\b", "")
+                     .Replace("\a", "")
+                     .Replace("\f", "")
+                     .Replace("\v", "")
+                     .Trim();
+            return str;
         }
     }
 }
